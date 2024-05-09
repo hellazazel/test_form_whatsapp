e73a51dd1a98ef9c6ab52c90d32ae361b660d838
hellazazel <worcaut2014@gmail.com>
2024-05-09 04:40:21 +0300

    recreate_form_vacancy

--- js/form.js
+++ js/form.js
@@ -4,39 +4,32 @@ document.querySelector("#submit").addEventListener("click", e => {
   //ВВЕДИТЕ ДЕЙСТВУЮЩИЙ НОМЕР WHATSAPP:
   const phone__number = "+7-921-978-61-59";
 
-  const client = document.querySelector("#client").value; // переменная 
-  const date = document.querySelector("#date").value;
-  const time = document.querySelector("#time").value;
-  const service = document.querySelector("#service").value;
-  const file = document.querySelector("#file").value;
+  const master = document.querySelector("#master").value; // переменная 
+  const old = document.querySelector("#old").value;
+  const car = document.querySelector("#car").value;
+  const experience = document.querySelector("#experience").value;
+  const cash = document.querySelector("#cash").value;
   const response = document.querySelector("#response");
 
   response.classList.remove("fail");
   response.classList.remove("send");
 
-  const url = `https://api.whatsapp.com/send?phone=${phone__number}&text=
-		*Заявка на сборку мебели*%0A
-		*Бронирование*%0A%0A
-		*Как вас зовут ?*%0A
-		${client}%0A
-		*Укажите желаемую дату сборку мебели*%0A
-		${date}%0A
-		*Укажите желаемое время сборки мебели*%0A
-		${time}%0A
-		*Сообщение*%0A
-		${service}%0A
-    *Схемы, фото или рисунки мебели*%0A
-    ${file}%0A`;
+  const url = `https://api.whatsapp.com/send?phone=
+  ${phone__number}&text=*Вакансия сборщика мебели*%0A*Резюме*%0A
+  %0A*Как вас зовут ?*%0A${master}
+  %0A*Сколько вам лет*%0A ${old}
+  %0A*У вас есть автомобиль*%0A ${car}
+  %0A*Ваш опыт работы ${experience}
+  %0A*Желаемая зарплата*%0A ${cash}`;
 
-  if (client === "" || date === "" || time === "") {
+  if (master === "" || old === "" || car === "" || experience === "") {
     response.classList.add("fail");
-    response.innerHTML = `Некоторые данные отсутствуют, ${client}`;
+    response.innerHTML = `Некоторые данные отсутствуют, ${master}`;
     return false;
   }
   response.classList.remove("fail");
   response.classList.add("send");
-  response.innerHTML = `Ваша заявка отправлена, ${client}`;
+  response.innerHTML = `Ваша заявка отправлена, ${master}`;
 
   window.open(url);
 });
-
